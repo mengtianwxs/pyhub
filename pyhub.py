27b39deefc797095928e4487cea631ac1948cda2
mengtianwxs <374835063@qq.com>
2016-12-04 17:01:44 +0800

    @mt

--- pyhub.py
+++ pyhub.py
@@ -3,6 +3,4 @@ import sys
 def config():
     reload(sys)
     sys.setdefaultencoding('utf-8')
-    print 'pyhub lib is running!'
-    print 'encodig is '+str(sys.getdefaultencoding())
-        
+    print 'pyhub lib is running! and encoding is %s'% str(sys.getdefaultencoding())
